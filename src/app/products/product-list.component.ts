2edbf5a80828c03249aea67c7b1fa316092d387c
Bimal Varghese <bimal.varghese@emnicon-local.de>
2018-11-08 14:59:48 +0530

    Moved product list from Components to ProductServices

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
@@ -24,52 +25,11 @@ export class ProductListComponent implements OnInit{
     }
 
     filteredProducts: IProduct[] = [];
-    products: IProduct[]=[
-        {
-            "productId":2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "Test description0",
-            "price": 32.99,
-            "starRating":4.2,
-            "imageUrl":"https://openclipart.org/image/300px/svg_to_png/58471/garden-cart.png"
-        },
-        {
-            "productId":3,
-            "productName": "Garden Cart 123",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "Test description0",
-            "price": 32.99,
-            "starRating":4.8,
-            "imageUrl":"https://openclipart.org/image/300px/svg_to_png/58471/garden-cart.png"
-        },
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-          },
-          {
-            "productId": 4,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-          },
-    ];
+    products: IProduct[]=[];
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'car';
+    // private _productService
+    constructor(private productService: ProductService) {
+        // this.listFilter = 'car';
     }
     toggleImage(): void {
         this.showImage =!this.showImage;
@@ -85,6 +45,9 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
+
         console.log("Inside OnInit");
     }
 
